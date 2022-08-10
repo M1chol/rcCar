e9f001ee5d330cf7d5a965fe4b2e80fd4b164593
M1chol <106252516+M1chol@users.noreply.github.com>
2022-08-10 18:57:05 +0200

    Update samochod.ino

--- samochod.ino
+++ samochod.ino
@@ -1,8 +1,6 @@
 #include <Servo.h>
 #include "Car.h"
 
-//piny
-
 Car car;
 
 //dioda do debugowania
@@ -18,14 +16,31 @@ void diodeBlink(int num){
 //setup
 void setup() {
  Serial.begin(9600);
+ while(!Serial){;}
  car.Setup();
- Serial.println(car.pos);
- car.Test();
- 
+ car.Test(); 
 }
 
 //glowna petla
 void loop() {
-  
+  if (Serial.available()){
+    String cmd = Serial.readStringUntil(' ');
+    
+    if(cmd.startsWith("stop")){
+      car.Stop;
+    }
+    
+    else if(cmd.startsWith("move")){
+      int spe=Serial.parseInt();
+      int acc=Serial.parseInt();
+      car.Move(spe,acce);
+    }
+
+    else if(cmd.startsWith("turn")){
+      int ang=Serial.parseInt();
+      int spe=Serial.parseInt();
+      car.Turn(ang,spe);
+    }
+  }
   
 }
