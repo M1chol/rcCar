eb37f50e7c45019d0880a8679ee27ca40565396f
M1chol <106252516+M1chol@users.noreply.github.com>
2022-08-10 18:57:42 +0200

    Update Car.h

--- Car.h
+++ Car.h
@@ -1,9 +1,6 @@
 class Car{
 
-  //zmienne prywatne
-
-  //pinMode(LED_BUILTIN, OUTPUT);
-  
+  //zmienne prywatne  
   //silnik
   int R_EN = 2;
   int R_PWM = 3;
@@ -13,45 +10,42 @@ class Car{
   int L_IS = 7;
 
   //serwo
-  
-  Servo serv; // stworzenie objektu serwa
-  int SRW = 9; // pin do ktorego podlaczone jest serwo
+  Servo serv;                     // stworzenie objektu serwa
+  int SRW = 9;                    // pin do ktorego podlaczone jest serwo
 
   public:
     //zmienne publiczne
-    int pos = 90-serv_err; // aktualna pozycja serwa
-    int serv_err = 18; // kat serwa aby kola byly prosto
-    int carSpeed;
-    int startingSpeed=10;
+    int pos = 90-serv_err;        // aktualna pozycja serwa
+    int serv_err = 18;            // kat serwa aby kola byly prosto
+    int carSpeed;                 // predkosc pojazdu
+    int startingSpeed=10;         // predkosc minimalna
   
     void Setup(){
       Serial.println("Starting");
       pinMode(SRW, OUTPUT);
-      serv.attach(SRW); // podlaczenie serwa
-      serv.write(pos); //poczatkowa pozycja kol
-      pinMode(R_IS, OUTPUT);
-      pinMode(R_EN, OUTPUT);
-      pinMode(R_PWM, OUTPUT); // obroty przod 0-256
-      pinMode(L_IS, OUTPUT); //
-      pinMode(L_EN, OUTPUT); //
-      pinMode(L_PWM, OUTPUT); // obroty tyl 0-256
+      serv.attach(SRW);           // podlaczenie serwa
+      pinMode(R_IS, OUTPUT);      // wyjscie alarmu
+      pinMode(R_EN, OUTPUT);      // zgoda na obrot
+      pinMode(R_PWM, OUTPUT);     // obroty przod 0-256
+      pinMode(L_IS, OUTPUT);      // wyjscie alarmu
+      pinMode(L_EN, OUTPUT);      // zgoda na obrot
+      pinMode(L_PWM, OUTPUT);     // obroty tyl 0-256
       digitalWrite(R_IS, LOW);
       digitalWrite(L_IS, LOW);
       digitalWrite(R_EN, HIGH);
       digitalWrite(L_EN, HIGH);
-      carSpeed = 0;
+      carSpeed = 0;               // poczatkowa predkosc auta
+      serv.write(pos);            // poczatkowa pozycja kol
     }
       
     void Turn(int angle, int turnSpeed){
       angle-=serv_err;
       if(angle==pos){
-        Serial.println(pos+serv_err);
         return;
       }
       if(turnSpeed == 0){
         serv.write(angle);
         pos=angle;
-        Serial.println(pos+serv_err);
         return;
       }
       int dist=abs(angle-pos);
@@ -68,11 +62,9 @@ class Car{
           delay(turnTime);
         }
       }
-      Serial.println(pos+serv_err);
     }
 
      void Stop(){
-      Serial.println("Stoping");
       analogWrite(R_PWM, 0);
       analogWrite(L_PWM, 0);
       carSpeed=0;
@@ -93,20 +85,13 @@ class Car{
     
     void Move(int toSpeed, int acceleration){
       
-      int toDir=toSpeed/abs(toSpeed);
-      if(toDir==1){
-        Serial.println("Moving Forward");
-      }else{
-        Serial.println("Moving Backward");
-      }
-      
+      int toDir=toSpeed/abs(toSpeed);      
       int acceForm=500/acceleration;
       
       if(carSpeed==0){
         WriteSpeed(startingSpeed*toDir);
       }
       if(toSpeed == carSpeed){
-        Serial.println("Same speed");
         return;
       }
       int dir=toDir;
@@ -129,12 +114,10 @@ class Car{
       }
       if (carSpeed!=toSpeed){
         WriteSpeed(toSpeed);
-        Serial.println("Div_err, snapping speed");
       }
     }
+    
     void Test(){
-      Serial.print("Testing Servo");
-      delay(1000);
       Turn(60,0);
       delay(500);
       Turn(90,0);
@@ -149,16 +132,12 @@ class Car{
       delay(1000);
       Turn(90,0);
       delay(1000);
-
-      Serial.println("Testing Engine");
-      Move(50,5);
-      delay(200);
-      Move(20,2);
-      delay(1000);
+      Move(30,5);
+      delay(2000);
       Stop();
       delay(2000);
-      Move(-30,2);
-      delay(3000);
+      Move(-30,5);
+      delay(2000);
       Stop();
     }
 };
